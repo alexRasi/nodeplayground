86eff2fe5d26f2b81b4840c55b9b250d9e12fa1f
Alexandros Rasidakis <alex.rasi94@gmail.com>
2023-12-09 11:49:40 +0100

    .

--- app.js
+++ app.js
@@ -20,9 +20,13 @@ app.get("/", function (request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+// // listen for requests :)
+// const listener = app.listen(process.env.PORT, function () {
+//   console.log("Your app is listening on port " + listener.address().port);
+// });
+
+const listener = app.listen(3000, function () {
+  console.log("Your app is listening on port " + 3000);
 });
 
 app.get("/hello", (req, res) => {
