68a31c31f5867779226ee84ae486fe945d3db584
Alexandros Rasidakis <alex.rasi94@gmail.com>
2023-12-10 06:07:44 +0100

    add rate limiter

--- app.js
+++ app.js
@@ -5,6 +5,17 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const rateLimit = require('express-rate-limit');
+
+// Apply to all requests
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  message: 'Too many requests from this IP, please try again after 15 minutes'
+});
+
+app.use(limiter);
+
 
 // Enable CORS for all routes and origins
 app.use(cors());
