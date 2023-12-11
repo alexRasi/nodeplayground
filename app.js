118fc3e62c03f77669d5dbe3fe4b679dd95f3218
Alexandros Rasidakis <alex.rasi94@gmail.com>
2023-12-11 18:38:08 +0100

    update cors

--- app.js
+++ app.js
@@ -11,15 +11,14 @@ const allowedDomains = ['https://quiz-alexrasi94.vercel.app', 'https://*.alexras
 
 app.use(cors({
   origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
-
-    if (allowedDomains.indexOf(origin) === -1) {
+    if (origin === 'https://quiz-alexrasi94.vercel.app' || origin.endsWith('.alexrasi94.vercel.app')) {
+      return callback(null, true);
+    } else {
       var msg = 'The CORS policy for this site does not ' +
                 'allow access from the specified Origin.';
       return callback(new Error(msg), false);
     }
-    return callback(null, true);
   }
 }));
 
