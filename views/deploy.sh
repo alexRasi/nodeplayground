da7836eedd3f00fccd47f19526cb961eff2da893
Alexandros Rasidakis <alex.rasi94@gmail.com>
2023-12-11 18:18:29 +0100

    add deploy script and notes

---
+++ views/deploy.sh
@@ -0,0 +1,11 @@
+# Checkout the main branch
+git checkout main
+
+# Pull the latest changes
+git pull
+
+# Install npm packages
+npm install
+
+# Restart the application using PM2
+pm2 restart app_name_or_id
