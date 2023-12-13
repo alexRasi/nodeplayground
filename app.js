a6e1f7bc615ad36e89f9cdbb95de4bcf8a592689
Alexandros Rasidakis <alex.rasi94@gmail.com>
2023-12-14 00:43:08 +0100

    change rate limit

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const app = express();
 // Apply to all requests
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  max: 1000, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
   message: "Too many requests from this IP, please try again after 15 minutes",
 });
 
